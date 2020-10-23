eafd683c2de1fa47b7cd5465c5cc9514a221ae61
injecti0n <9992685+injecti0n@users.noreply.github.com>
2020-10-23 17:16:39 +0200

    updated

---
+++ app.js
@@ -0,0 +1,16 @@
+const cell = document.querySelectorAll('.honeycomb-cell');
+const modal_title = document.getElementById('title');
+const modal_body = document.querySelector('.modal-body');
+
+cell.forEach(cell => {
+    
+    cell.setAttribute('data-toggle', 'modal');
+    cell.setAttribute('data-target', '#info')
+
+    cell.addEventListener('click', (event) => {
+        console.log(event.target.innerHTML)
+        
+        modal_title.innerHTML= event.target.innerHTML
+    })
+
+})
